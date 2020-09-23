5d45afd963d17281fc906c0ec3abddb3bb290d1e
yohana tesfamariam <66391900+Yohana-Cyber15@users.noreply.github.com>
2020-09-23 13:26:55 -0700

    Add files via upload

---
+++ GuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.cyber.guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
